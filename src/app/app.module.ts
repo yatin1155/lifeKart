f53124b5b88a0cfce34d90aa40161128affd0a68
yatin1155 <yatinkapur@gmail.com>
2019-03-18 16:27:49 +0530

    routing complete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,8 +6,15 @@ import { BsNavBarComponent } from './bs-nav-bar/bs-nav-bar.component';
 import { HomeComponent } from './home/home.component';
 import { ProductsComponent } from './products/products.component';
 import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+import { CheckOutComponent } from './check-out/check-out.component';
+import { OrderSuccessComponent } from './order-success/order-success.component';
+import { MyOrdersComponent } from './my-orders/my-orders.component';
+import { AdminProductsComponent } from './admin/admin-products/admin-products.component';
+import { AdminOrdersComponent } from './admin/admin-orders/admin-orders.component';
+import { LoginComponent } from './login/login.component';
 
-
+import { RouterModule } from "@angular/router";
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap"
 
 @NgModule({
   declarations: [
@@ -15,10 +22,28 @@ import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
     BsNavBarComponent,
     HomeComponent,
     ProductsComponent,
-    ShoppingCartComponent
+    ShoppingCartComponent,
+    CheckOutComponent,
+    OrderSuccessComponent,
+    MyOrdersComponent,
+    AdminProductsComponent,
+    AdminOrdersComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule.forRoot(),
+    RouterModule.forRoot([
+      { path: "", component: HomeComponent },
+      { path: "products", component: ProductsComponent },
+      { path: "shopping-cart", component: ShoppingCartComponent },
+      { path: "check-out", component: CheckOutComponent },
+      { path: "order-success", component: OrderSuccessComponent },
+      { path: "my/orders", component: MyOrdersComponent },
+      { path: "login", component: LoginComponent },
+      { path: "admin/products", component: AdminProductsComponent },
+      { path: "admin/orders", component: AdminOrdersComponent }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
